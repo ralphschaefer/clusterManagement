2cdb9b967c16ea5f45c8efad1af27d7715843a9d
Ralph Schäfer <ralph.schaefer@emnify.com>
2018-05-04 11:47:37 +0200

    wip

--- src/main/scala/my/seedless/Main.scala
+++ src/main/scala/my/seedless/Main.scala
@@ -1,11 +1,25 @@
 package my.seedless
 
+import akka.discovery.SimpleServiceDiscovery.Resolved
+
+import scala.util.Success
+
 object Main extends App {
   println("startup ...")
 
-  val nodeInfo = NodeInfo("mangementTest")
+  val nodeInfo = NodeInfo("managementTest")
 
   println("running ...")
+
+
+  import scala.concurrent.ExecutionContext.Implicits.global
+  nodeInfo.servicelookup.onComplete{
+    case Success(Resolved(serviceName,list)) =>
+      println(s"$serviceName -> ${list.mkString(",")}")
+    case _ =>
+      println("no service found")
+  }
+
   println("ANY Key?")
   scala.io.StdIn.readLine()
 
--- src/main/scala/my/seedless/NodeInfo.scala
+++ src/main/scala/my/seedless/NodeInfo.scala
@@ -1,17 +1,23 @@
 package my.seedless
 
 import akka.actor.{ActorRef, ActorSystem, CoordinatedShutdown}
+import akka.discovery.SimpleServiceDiscovery.Resolved
+import akka.discovery.{ServiceDiscovery, SimpleServiceDiscovery}
 import akka.management.AkkaManagement
 import com.typesafe.config.Config
+import scala.concurrent.duration._
+
+import scala.concurrent.Future
 
 trait NodeInfo {
   val system: ActorSystem
   val config: Config
   val httpClusterManagement: AkkaManagement
+  val discovery: SimpleServiceDiscovery
   def shutdown():Unit = {
     import scala.concurrent.ExecutionContext.Implicits.global
     httpClusterManagement.stop().onComplete(
-      _ => println("cluster Management stopped")
+       _ => println("cluster Management stopped")
     )
     CoordinatedShutdown.get(system).run(CoordinatedShutdown.unknownReason)
   }
@@ -22,5 +28,7 @@ object NodeInfo {
     val system: ActorSystem = ActorSystem(actorSystemName)
     lazy val config: Config = system.settings.config
     lazy val httpClusterManagement = AkkaManagement(system)
+    lazy val discovery = ServiceDiscovery(system).discovery
+    lazy val servicelookup: Future[Resolved] = discovery.lookup("nodes.mycluser.local", resolveTimeout = 500 milliseconds)
   }
 }
